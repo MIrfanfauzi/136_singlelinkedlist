3f0e2dadfcc981ec87895bdca6fd03ccafa7af0e
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-04-26 10:21:42 +0700

    deklarasi fungsi search

--- test.cpp
+++ test.cpp
@@ -48,3 +48,15 @@ void addNode(){
     nodeBaru->next = current;
     previous->next = nodeBaru; 
 }
+
+bool searchNode(int nim, Node* current, Node* previous) {
+    previous = START;
+    current = START;
+    while (current !=NULL && nim > current->noMhs)
+    {
+        previous = current;
+        current = current->next;
+    }
+
+   
+}
