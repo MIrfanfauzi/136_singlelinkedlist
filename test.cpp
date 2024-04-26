4950867fe77636a18a6f2e64233acf3362305d2c
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-04-26 10:10:53 +0700

    menyimpan node sebelumnya dan maju ke node berikutnya

--- test.cpp
+++ test.cpp
@@ -41,5 +41,9 @@ void addNode(){
             cout << "NIM sudah ada " << endl;
             return;
         }
-    }     
+        previous = current;
+        current = current->next;
+    }
+
+    
 }
